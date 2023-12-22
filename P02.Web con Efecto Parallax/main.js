ef8518582bb74edec14fbbb6d534e6bd34cdd24a
Johnny <jonathanbc2012@gmail.com>
2023-12-22 17:30:21 +0100

    updated

--- P02.Web con Efecto Parallax/main.js
+++ P02.Web con Efecto Parallax/main.js
@@ -18,11 +18,11 @@ window.onload = function() {
     lax.addElements('.logo', {
         scrollY: {
             scale:[
-                [0, 80],
+                [0, 600], /* posición de nuestro scroll */
                 [1, 3],
             ],
             opacity:[
-                [0, 70],
+                [0, 380],
                 [1, 0],
             ],
             translateY:[
@@ -36,46 +36,46 @@ window.onload = function() {
     lax.addElements('.ultron', {
         scrollY: {
             opacity:[
+                [0, 200],
+                [0, 1],
+            ],
+            translateY:[
+                [0, 400],
                 [0, 80],
-                [0, 1],
-            ],
-            translateY:[
-                [0, 400],
-                [0, 75],
             ],
         },
     });
 
-    lax.addElements('.loki', {
+     lax.addElements('.loki', {
         scrollY: {
             opacity:[
-                [40, 120],
+                [240, 320],
                 [0, 1],
             ],
             translateY:[
                 [0, 400],
-                [0, -75],
+                [0, -80],
             ],
         },
     });
-
-    lax.addElements('.thanos', {
+ 
+     lax.addElements('.thanos', {
         scrollY: {
             opacity:[
-                [80, 160],
+                [280, 360],
                 [0, 1],
             ],
             translateY:[
                 [0, 400],
-                [0, 75],
+                [0, 80],
             ],
         },
     });
-
-    lax.addElements('.scroll-tip', {
+ 
+     lax.addElements('.scroll-tip', {
         scrollY: {
             opacity:[
-                [0, 600],
+                [0, 80],
                 [1, 0],
             ],
             translateY:[
@@ -90,12 +90,12 @@ window.onload = function() {
             ],
             
         },
-    });
+    }); 
 
-    lax.addElements('.ironman-title', {
+     lax.addElements('.ironman-title', {
         scrollY: {
             filter:[
-                [600, 700, 800, 900, 1000, 1100, 1200],
+                [1000, 1100, 1200, 1300, 1400, 1500, 1600],
                 [0, 50, 0, 50, 0, 75, 0],
                 {
                     cssFn: function(value){
@@ -105,9 +105,49 @@ window.onload = function() {
             ],      
                        
             translateY:
-            [[1200, 1700],
-            [200, 'elCenterY-150']] /* el CenterY es una propiedad de lax  */
+            [[1400, 2800],
+            [200, "elCenterY-150"]] /* el CenterY es una propiedad de lax  */
         },
     });
+ 
+     lax.addElements('.ironman-01', {
+        scrollY: {                     
+            translateY:
+            [
+            [600, 2000],
+            [800, -1500]
+            ] 
+        },
+    }); 
+
+      lax.addElements('.ironman-02', {
+        scrollY: {   
+            scale: [
+                [2000, 2500],
+                [0, 1.5]
+            ],                  
+            translateX: [
+                [2000, 2200, 3000],
+                [0, 1000, 0]
+            ],
+            translateY: [
+                [2000, 2200, 3000],
+                [-500, 0, -400]
+            ] 
+        },
+    });  
+      lax.addElements('.shield', {
+        scrollY: {                               
+            translateX: [
+                [3200, 3400, 3600, 3800],
+                [-800, 'screenWidth - 200', 0, 'screenWidth + 200']
+            ],
+            translateY: [
+                [3200, 3400, 3600, 3800],
+                [800, 1200, 1600, 1600]
+            ],
+            
+        },
+    });  
 };
 
